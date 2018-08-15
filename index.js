4b379483ce70b1a320e63be030754ff5f4c222cb
Lauren West <laurenwest12@gmail.com>
2018-08-15 00:02:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver () {
 }
 
 function appendDriver (name){
-  let newArray = [drivers, ..."Broom"]
+  let newArray = [...drivers, "Broom"]
   return newArray
 }
